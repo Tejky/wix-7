ed6705b53f095e86bd727df5b7af5727c7138d3a
Teodor Sepeši <teodorsepesi@gmail.com>
2026-05-20 21:28:13 +0200

    Fix build compatibility with VS 2026 Insiders

---
+++ src/ext/ext.cmd
@@ -0,0 +1,16 @@
+@setlocal
+@pushd %~dp0
+
+@set _C=Debug
+@set _L=%~dp0..\..\build\logs
+@set _SuppressWixClean=false
+
+:parse_args
+@if /i "%1"=="release" set _C=Release
+@if /i "%1"=="inc" set _SuppressWixClean=true
+@if not "%1"=="" shift & goto parse_args
+
+msbuild ext_t.proj -p:Configuration=%_C% -p:SuppressWixClean=%_SuppressWixClean% -p:VCToolsVersion=14.44.35207 -p:PlatformToolset=v143 -m -tl -nologo -warnaserror -bl:%_L%\ext_build.binlog || exit /b
+
+@popd
+@endlocal
